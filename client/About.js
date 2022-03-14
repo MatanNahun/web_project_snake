7fc5a6ee5a1dfdfd6676a132c50396cdaa526638
matan <nahum131313@gmail.com>
2022-03-14 17:54:14 +0200

    first commit

---
+++ client/About.js
@@ -0,0 +1,40 @@
+
+let mail = document.getElementById("email");
+let phone = document.getElementById("phone");
+
+
+
+function ValidateEmail() {
+let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+if(mail.value.match(mailformat) && (phone.value.length = 10)) {
+alert("thank you partner!");
+}
+
+else {
+alert("You have entered an invalid email address or phone number!");
+}
+}
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTop = 0; 
+}
+
+
+
+
+
